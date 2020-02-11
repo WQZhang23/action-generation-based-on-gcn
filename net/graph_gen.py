f83a90a4ad2fab1d5f6ad0d13dce3d91d9861812
wuqiang <1185327703@qq.com>
2020-02-11 22:15:14 +0800

    finish the configiration of generator and discriminator

--- net/graph_gen.py
+++ net/graph_gen.py
@@ -14,6 +14,169 @@ class Generator(nn.Module):
                  edge_importance_weighting, **kwargs):
         super().__init__()
 
+        # load graph
+        self.graph = Graph(**graph_args)
+        A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
+        self.register_buffer('A', A)
+
+        # build networks
+        spatial_kernel_size = A.size(0)
+        temporal_kernel_size = 9
+        kernel_size = (temporal_kernel_size, spatial_kernel_size)
+        self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
+        kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
+
+
+        self.gen_stage1 = nn.ModuleList((
+            st_gen_conv(in_channels, 64, kernel_size, 1, residual=False, **kwargs0),
+            st_gen_conv(64, 64, kernel_size, 1, **kwargs),
+            st_gen_conv(64, 128, kernel_size, 2, **kwargs),
+            st_gen_conv(128, 128, kernel_size, 1, **kwargs),
+            st_gen_conv(128, 256, kernel_size, 2, **kwargs),
+            st_gen_conv(256, 256, kernel_size, 1, **kwargs),
+            st_gen_conv(256, 256, kernel_size, 1, **kwargs),
+            st_gen_deconv(256, 128, kernel_size, 1, **kwargs),
+            st_gen_conv(128, 128, kernel_size, 1, **kwargs),
+            st_gen_deconv(128, 64, kernel_size, 1, **kwargs),
+            st_gen_conv(64, 48, kernel_size, 1, **kwargs),
+            st_gen_conv(48, in_channels, kernel_size, 1, **kwargs),
+        ))
+
+        self.gen_stage2 = nn.ModuleList((
+            st_gen_conv(in_channels, 64, kernel_size, 1, residual=False, **kwargs0),
+            st_gen_conv(64, 64, kernel_size, 1, **kwargs),
+            st_gen_conv(64, 128, kernel_size, 2, **kwargs),
+            st_gen_conv(128, 256, kernel_size, 1, **kwargs),
+            st_gen_conv(256, 128, kernel_size, 1, **kwargs),
+            st_gen_deconv(128, 64, kernel_size, 1, **kwargs),
+            st_gen_conv(64, 48, kernel_size, 1, **kwargs),
+            st_gen_conv(48, in_channels, kernel_size, 1, **kwargs),
+        ))
+
+        # initialize parameters for edge importance weighting
+        if edge_importance_weighting:
+            self.edge_importance_stage1 = nn.ParameterList([
+                nn.Parameter(torch.ones(self.A.size()))
+                for i in self.gen_stage1
+            ])
+            self.edge_importance_stage2 = nn.ParameterList([
+                nn.Parameter(torch.ones(self.A.size()))
+                for i in self.gen_stage2
+            ])
+        else:
+            self.edge_importance_stage1 = [1] * len(self.gen_stage1)
+            self.edge_importance_stage2 = [1] * len(self.gen_stage2)
+
+        # fcn for prediction
+        self.fcn = nn.Conv2d(256, num_class, kernel_size=1)
+
+    def forward(self, x, mask):
+
+        # data normalization
+        N, C, T, V, M = x.size()
+        x = x.permute(0, 4, 3, 1, 2).contiguous()
+        x = x.view(N * M, V * C, T)
+        x = self.data_bn(x)
+        x = x.view(N, M, V, C, T)
+        x = x.permute(0, 1, 3, 4, 2).contiguous()
+        x_pos = x.view(N * M, C, T, V)
+
+        x_ori = x_pos
+
+        # here x is original input data
+        # use y and mask to config the input data for the model
+        y = torch.ones_like(x_pos)[:, 0:1, :, :]
+
+        N_m, C_m, T_m, V_m, M_m = mask.size()
+        mask = mask.view(N_m * M_m, C_m, T_m, V_m)
+
+        x = torch.cat((x_pos, y, mask), 1)
+
+        # stage 1 forward
+        for gcn, importance in zip(self.gen_stage1, self.edge_importance_stage1):
+            x, _ = gcn(x, self.A * importance)
+        x_stage1 = nn.Tanh(x)
+
+
+        # stage 2 forward
+        x = x_stage1*mask + x_ori*(1-mask)
+        pdb.set_trace()
+        for gcn, importance in zip(self.gen_stage2, self.edge_importance_stage2):
+            x, _ = gcn(x, self.A * importance)
+
+        x_stage2 = nn.Tanh(x)
+
+        x_stage1_reshape = x_stage1.view(N, M, C, T, V)
+        x_stage1_reshape = x_stage1_reshape.permute(0,2,3,4,1)
+
+        x_stage2_reshape = x_stage2.view(N, M, C, T, V)
+        x_stage2_reshape = x_stage2_reshape.permute(0,2,3,4,1)
+
+        return x_stage1_reshape, x_stage2_reshape
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, num_class, graph_args,
+                 edge_importance_weighting, **kwargs):
+        super().__init__()
+
+        # load graph
+        self.graph = Graph(**graph_args)
+        A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
+        self.register_buffer('A', A)
+
+        # build networks
+        spatial_kernel_size = A.size(0)
+        temporal_kernel_size = 9
+        kernel_size = (temporal_kernel_size, spatial_kernel_size)
+        self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
+        kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
+        self.st_gcn_networks = nn.ModuleList((
+            st_gcn(in_channels, 64, kernel_size, 1, residual=False, **kwargs0),
+            st_gcn(64, 64, kernel_size, 1, **kwargs),
+            st_gcn(64, 128, kernel_size, 2, **kwargs),
+            st_gcn(128, 128, kernel_size, 1, **kwargs),
+            st_gcn(128, 256, kernel_size, 2, **kwargs),
+            st_gcn(256, 256, kernel_size, 1, **kwargs),
+        ))
+
+        # initialize parameters for edge importance weighting
+        if edge_importance_weighting:
+            self.edge_importance = nn.ParameterList([
+                nn.Parameter(torch.ones(self.A.size()))
+                for i in self.st_gcn_networks
+            ])
+        else:
+            self.edge_importance = [1] * len(self.st_gcn_networks)
+
+        # fcn for prediction
+        self.fcn = nn.Conv2d(256, 2, kernel_size=1)
+
+
+    def forward(self, x):
+
+        # data normalization
+        N, C, T, V, M = x.size()
+        x = x.permute(0, 4, 3, 1, 2).contiguous()
+        x = x.view(N * M, V * C, T)
+        x = self.data_bn(x)
+        x = x.view(N, M, V, C, T)
+        x = x.permute(0, 1, 3, 4, 2).contiguous()
+        x_pos = x.view(N * M, C, T, V)
+
+        # forwad
+        for gcn, importance in zip(self.st_gcn_networks, self.edge_importance):
+            x, _ = gcn(x, self.A * importance)
+
+        # global pooling
+        x = F.avg_pool2d(x, x.size()[2:])
+        x = x.view(N, M, -1, 1, 1).mean(dim=1)
+
+        # prediction
+        x = self.fcn(x)
+        x = x.view(x.size(0), -1)
+
+        return x
 
 
 class Model(nn.Module):
@@ -35,7 +198,6 @@ class Model(nn.Module):
             :math:`V_{in}` is the number of graph nodes,
             :math:`M_{in}` is the number of instance in a frame.
     """
-
     def __init__(self, in_channels, num_class, graph_args,
                  edge_importance_weighting, **kwargs):
         super().__init__()
@@ -76,6 +238,7 @@ class Model(nn.Module):
         # fcn for prediction
         self.fcn = nn.Conv2d(256, num_class, kernel_size=1)
 
+
     def forward(self, x, mask):
 
         # data normalization
@@ -100,16 +263,6 @@ class Model(nn.Module):
         # forwad
         for gcn, importance in zip(self.st_gcn_networks, self.edge_importance):
             x, _ = gcn(x, self.A * importance)
-
-
-        # gen_conv
-
-
-        # gen_dconv
-
-
-        # dic
-
 
         # global pooling
         x = F.avg_pool2d(x, x.size()[2:])
@@ -281,9 +434,14 @@ class st_gen_conv(nn.Module):
         # x_incom = x * (1-mask)
         # mask = nn.Sigmoid(mask)
 
+        a,b,T,N = x.size()
+
         res = self.residual(x)
         x, A = self.gcn(x, A)
         x = self.tcn(x) + res
+
+        if b == 3:
+            return x, A
 
         x, y = torch.split(x, 2, 1)
         x = self.relu(x)
@@ -351,6 +509,8 @@ class st_gen_deconv(nn.Module):
         res = self.residual(x)
         x, A = self.gcn(x, A)
         x = self.tcn(x) + res
+        if b == 3:
+            return x, A
 
         x, y = torch.split(x, 2, 1)
         x = self.relu(x)
